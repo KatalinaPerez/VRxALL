104cfc51367dda3f4153454d1cbac13d1355db35
KatalinaPerez <kata.perezg@duocuc.cl>
2025-09-26 20:22:08 -0300

    home.html, nad y foot listos, falta mejorar body

--- VRxALLp/vrxallapp/urls.py
+++ VRxALLp/vrxallapp/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.home, name='home')  # Esto apunta a home.html
+    path('', views.home, name='home'),
+    path('aplicaciones/<slug:slug>/', views.detalles_app, name='detalles_app'),
 ]
