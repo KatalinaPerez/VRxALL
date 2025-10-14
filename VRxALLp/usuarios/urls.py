bc8ae798f4f760976a330f63f7d057ea0b0381db
Bvnjjaaa <ben.romero@duocuc.cl>
2025-10-14 20:13:03 -0300

    login terminado y registro en curso, falta api para validacion de mail

---
+++ VRxALLp/usuarios/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views  
+from django.contrib.auth import views as auth_views
+from .views import logout_get_view 
+
+urlpatterns = [
+    path('login/', views.login_view, name='login'),
+     path('logout/', logout_get_view, name='logout'),
+    path('registro/', views.registro_view, name='registro'),
+]
